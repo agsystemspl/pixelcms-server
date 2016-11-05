ca2a07f633014b16163d09e1b79350c8cb6e197c
Michal Werner <michal@hurtowniapixeli.pl>
2016-11-05 17:59:39 +0100

    API docs page only in debug

--- cms/common/urls.py
+++ cms/common/urls.py
@@ -9,7 +9,6 @@ from . import api_urls
 
 urlpatterns = [
     url(r'^api/', include(api_urls)),
-    url(r'^api-docs/', include('rest_framework_docs.urls')),
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^admin/grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls))
@@ -17,6 +16,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [
+        url(r'^api-docs/', include('rest_framework_docs.urls')),
         url(
             r'^media/(?P<path>.*)$', static_serve,
             {'document_root': settings.MEDIA_ROOT}
