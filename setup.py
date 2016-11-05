9b142f5ff3921123138ff75c869403daf8cfb37d
Michal Werner <michal@hurtowniapixeli.pl>
2016-11-05 17:51:30 +0100

    Version changed to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pixelcms-server',
-    version='0.2.1',
+    version='0.2.2',
     description='PixelCMS server part.',
     url='https://github.com/HurtowniaPixeli/pixelcms-server',
     author='Michał Werner',
