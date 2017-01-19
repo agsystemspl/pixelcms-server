332b0658f86d29534a90cbc98ab083305c720c1b
Michal Werner <michal@hurtowniapixeli.pl>
2017-01-19 14:48:41 +0100

    Version changes to 0.4.2

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,2 +1,2 @@
 # (major, minor, patch, pre-release type, pre-release number)
-VERSION = (0, 4, 1, None, None)
+VERSION = (0, 4, 2, None, None)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pixelcms-server',
-    version='0.4.1',
+    version='0.4.2',
     description='PixelCMS server part.',
     url='https://github.com/HurtowniaPixeli/pixelcms-server',
     author='Michał Werner',
